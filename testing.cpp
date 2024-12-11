333ffd227eaaa4e81a603285c2972758f8221174
Bennett Silverman <152738859+BSilverman6@users.noreply.github.com>
2024-12-11 10:54:49 -0700

    commented and cleaned up header files

--- testing.cpp
+++ testing.cpp
@@ -9,16 +9,6 @@
 
 using namespace std;
 
-/*Color Codes
-Black 0
-Red 31
-Green 2
-Yellow 3
-Blue 4
-Magenta 5
-Cyan 6
-White 7
-*/
 #define RESET   "\033[0m"
 
 
@@ -28,8 +18,9 @@ int main(){
     my_grid->setUpMap(9);
 
     vector <Tile*> short_path = my_grid->getShortestPath(4,0,4,8);
-    //my_grid->print_grid();
     cout << "Hello World" << RESET <<endl;
+    cout << 14*1.5 <<endl;
+
     return 0;
 }
 
