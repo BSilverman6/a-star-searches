a5dd1674ebd44b38d81bbb4901fa5142b4dc33bf
Bennett Silverman <152738859+BSilverman6@users.noreply.github.com>
2024-12-11 09:14:21 -0700

    fixed bug when end isn't accessible, added bramble labels. brambles tested

--- testing.cpp
+++ testing.cpp
@@ -4,15 +4,32 @@
 #include "Tile.cpp"
 
 
+#include <stdlib.h> 
 #include <iostream>
 
 using namespace std;
 
+/*Color Codes
+Black 0
+Red 31
+Green 2
+Yellow 3
+Blue 4
+Magenta 5
+Cyan 6
+White 7
+*/
+#define RESET   "\033[0m"
+
+
 int main(){
     Grid* my_grid = new Grid();
 
-    my_grid->setUpMap(5);
+    my_grid->setUpMap(9);
 
-    cout << "Hello World" <<endl;
+    vector <Tile*> short_path = my_grid->getShortestPath(4,0,4,8);
+    //my_grid->print_grid();
+    cout << "Hello World" << RESET <<endl;
     return 0;
 }
+
